5816a63b038cd9767cef01e8e5d55bd351492bd0
yorranan <yorranan.costa@outlook.com>
2023-09-11 12:27:14 -0300

    first html code of home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FirebaseService } from '../model/services/firebase.service';
+import { Movie } from '../model/entities/Movie';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
+title!: string;
+genres: string[] = []
 
-  constructor() {}
+
+  constructor(
+    private router: Router,
+    private firebase: FirebaseService
+  ) {}
+
+  read() {
+    
+  }
 
 }
