e62ac3efb1c85a18b0c5538f7944c7e7d186b00a
yorranan <yorranan.costa@outlook.com>
2023-11-21 16:44:10 -0300

    created downloadURL in Movie's atributes

--- src/app/model/entities/Movie.ts
+++ src/app/model/entities/Movie.ts
@@ -5,50 +5,51 @@ export class Movie {
     private _writer: string;
     private _releaseDate: string;
     private _genres: string[] = [];
+	private _downloadURL: string = "";
 
     constructor(title: string, director: string, writer: string, date: string, genres: string[]) {
-        this._title = title;
+		this._title = title;
         this._director = director;
         this._writer = writer;
         this._releaseDate = date;
         this._genres = genres;
     }
-
+	
 	public get id(): string {
-        return this._id;
+		return this._id;
     }
     public set id(value: string) {
-        this._id = value;
+		this._id = value;
     }
 	
 	public get title(): string {
 		return this._title;
 	}
-
+	
 	public set title(value: string) {
 		this._title = value;
 	}
-
+	
 	public get director(): string {
 		return this._director;
 	}
-
+	
 	public set director(value: string) {
 		this._director = value;
 	}
-
+	
 	public get writer(): string {
 		return this._writer;
 	}
-
+	
 	public set writer(value: string) {
 		this._writer = value;
 	}
-
+	
 	public get releaseDate(): string {
 		return this._releaseDate;
 	}
-
+	
 	public set releaseDate(value: string) {
 		this._releaseDate = value;
 	}
@@ -60,5 +61,12 @@ export class Movie {
 	public set genres(value: string[] ) {
 		this._genres = value;
 	}
-
+	
+	public get downloadURL(): string {
+		return this._downloadURL;
+	}
+	
+	public set downloadURL(value: string) {
+		this._downloadURL = value;
+	}
 }
