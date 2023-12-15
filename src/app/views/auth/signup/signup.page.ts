7567cbbedab9c4a4638e02eadacc1cc4d1ae9ee2
yorranan <yorranan.costa@outlook.com>
2023-12-15 12:07:18 -0300

    signin and signup On branch main

--- src/app/views/auth/signup/signup.page.ts
+++ src/app/views/auth/signup/signup.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/common/alert.service';
+import { AuthService } from 'src/app/model/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +15,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  formCadastrar: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private authService: AuthService
+  ) {
+    this.formCadastrar = new FormGroup({
+      email: new FormControl(''),
+      senha: new FormControl(''),
+      confirmarSenha: new FormControl(''),
+    });
   }
 
+  ngOnInit() {
+    this.formCadastrar = this.formBuilder.group({
+      // o primeiro campo se refere ao campo que pode vir preenchido quando carregamos a tela ou vazio como está configurado no momento
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(8)]],
+      confirmarSenha: ['', [Validators.required]],
+    });
+  }
+
+  get erroControl() {
+    return this.formCadastrar.controls;
+  }
+
+  submitForm() {
+    if (!this.formCadastrar.valid) {
+      this.alertService.presentAlert('Erro', 'Campos Obrigatórios!');
+      return false;
+    } else {
+      this.cadastrar();
+      return true;
+    }
+  }
+
+  private cadastrar() {
+    this.authService
+      .signUpWithEmailAndPassword(
+        this.formCadastrar.value['email'],
+        this.formCadastrar.value['senha']
+      )
+      .then((res) => {
+        this.alertService.presentAlert(
+          'Sucesso',
+          'Cadastro realizado com sucesso!'
+        );
+        this.router.navigate(['signin']);
+      }).catch((error) => {
+        this.alertService.presentAlert('Erro', 'Erro ao cadastrar!');
+        console.log(error.message);
+      });
+  }
+
+  irParaSignIn() {
+    this.router.navigate(['signin']);
+  }
 }
