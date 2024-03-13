c79030582abffb2d761746c0dca7152c0d66999e
yorranan <yorranan.costa@outlook.com>
2024-03-13 13:51:11 -0300

    fix problem at search

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -10,8 +10,9 @@ import { AlertService } from 'src/app/model/services/alert.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage{
+export class HomePage {
   public movies: Movie[] = [];
+  movies_saved: Movie[] = [];
   query: any;
   isLoading: boolean = false;
   public user: any;
@@ -26,18 +27,30 @@ export class HomePage{
     private auth: AuthService,
     private alert: AlertService
   ) {
-    this.readUserInformation();
-  }
-  
-  onSearchChange(event: any) {
-    this.query = event.detail.value.toLowerCase(); 
     this.isLoading = true;
+    this.user = this.auth.getUserLogged();
+    this.firebase.read(this.user.uid).subscribe(res => {
+      this.movies = res.map(movie => {
+        return {
+          id: movie.payload.doc.id,
+          ...movie.payload.doc.data() as any
+        } as Movie;
+      })
+      this.movies_saved = this.movies;
+    });
+    this.isLoading = false;
+  }
+
+  onSearchChange(event: any) {
+    this.isLoading = true;
+    this.query = event.detail.value.toLowerCase();
     if (this.query.length > 0) {
-      this.movies = this.movies.filter(movie => 
+      this.movies = this.movies_saved;
+      this.movies = this.movies.filter(movie =>
         movie.title.toLowerCase().includes(this.query)
       );
     } else {
-      this.readUserInformation();
+      this.movies = this.movies_saved;
     }
     this.isLoading = false;
   }
@@ -45,24 +58,9 @@ export class HomePage{
   goToRegister() {
     this.router.navigate(["/register-movie"])
   }
-  
-  logout(){
+
+  logout() {
     this.alert.confirmLogOut(this.user.uid);
   }
 
-  async readUserInformation() {
-    this.isLoading = true;
-    this.user = this.auth.getUserLogged();
-    this.firebase.read(this.user.uid).subscribe(res =>{
-      this.movies = res.map(movie => {
-        return {
-          id: movie.payload.doc.id,
-          ...movie.payload.doc.data() as any
-        } as Movie;
-      })
-    })
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 1000);
-  }
 }
